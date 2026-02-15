4624e518b1f8ba24ff759b718e8d39dbdc8def82
apple <rohitdli1990@gmail.com>
2026-02-16 03:06:49 +0530

    GO

--- Main.cpp
+++ Main.cpp
@@ -364,13 +364,244 @@ public:
     }
 
     static float Weight() {
-        int a;
+        float a;
+        int Choose;
+        float result;
 
         cout<<"Welcome Weight Converter";
-        cout<<"1. Gram to ";
-        cout<<"2. ";
+        cout<<"1. Gram to Tonne";
+        cout<<"2. Gram to Kilogram";
+        cout<<"3. Gram to Milligram";
+        cout<<"4. Gram to Microgram";
+        cout<<"5. Gram to Quintal";
+        cout<<"6. Gram to Pound";
+        cout<<"7. Gram to Ounce";
+        cout<<"8. Gram to Carat";
+        cout<<"9. Gram to Liang";
+        cout<<"10. Gram to Dan";
+        cout<<"11. Gram to Jin";
+        cout<<"12. Gram to Qian";
+        cout<<"Enter your Choose:- ";
+        cin>>Choose;
+        cout<<"Enter Weight in gram:- ";
+        cin>>a;
 
+        switch (Choose) {
+            case 1:
+                result = a*0.000001;
+                cout<<"Your result is "<<result<<endl;
+            case 2:
+                result = a*0.001;
+                cout<<"Your result is "<<result<<endl;
+            case 3:
+                result = a*1000;
+                cout<<"Your result is "<<result<<endl;
+            case 4:
+                result = a*1000000;
+                cout<<"Your result is "<<result<<endl;
+            case 5:
+                result = a*0.00001;
+                cout<<"Your result is "<<result<<endl;
+            case 6:
+                result = a*0.002204622621848775;
+                cout<<"Your result is "<<result<<endl;
+            case 7:
+                result = a*0.035273961949580412;
+                cout<<"Your result is "<<result<<endl;
+            case 8:
+                result = a*5;
+                cout<<"your result is "<<result<<endl;
+            case 9:
+                result = a+0.02;
+                cout<<"your result is "<<result<<endl;
+            case 10:
+                result = a*0.00002;
+                cout<<"your result is "<<result<<endl;
+            case 11:
+                result = a*0.002;
+                cout<<"your result is "<<result<<endl;
+            case 12:
+                result = a*0.2;
+                cout<<"your result is "<<result<<endl;
+            default:
+                cout<<"Invaild input";
+        }
         return 0;
+    }
+
+    static float Area() {
+        float a;
+        int Choose;
+        float result;
+
+        cout<<"Welcome Area Converter";
+        cout<<"1. Square meter to Square kilometer";
+        cout<<"2. Square meter to Hectare";
+        cout<<"3. Square meter to Are";
+        cout<<"4. Square meter to Square decimeter";
+        cout<<"5. Square meter to Square centimeter";
+        cout<<"6. Square meter to Square millimeter";
+        cout<<"7. Square meter to Acre";
+        cout<<"8. Square meter to Square mile";
+        cout<<"9. Square meter to Square yard";
+        cout<<"10. Square meter to Square foot";
+        cout<<"11. Square meter to Square inch";
+        cout<<"12. Square meter to Square rod";
+        cout<<"Enter your Choose:- ";
+        cin>>Choose;
+        cout<<"Enter Area in Square Meter:- ";
+        cin>>a;
+
+        if (Choose==1) {
+            result = a*0.000001;
+            cout<<"your result is "<<result<<endl;
+        }
+        else if (Choose == 2) {
+            result = a*0.0001;
+            cout<<"your result is "<<result<<endl;
+        }
+        else if (Choose == 3) {
+            result = a*0.01;
+            cout<<"your result is "<<result<<endl;
+        }
+        else if (Choose == 4) {
+            result = a*100;
+            cout<<"your result is "<<result<<endl;
+        }
+        else if (Choose == 5) {
+            result = a*10000;
+            cout<<"your result is "<<result<<endl;
+        }
+        else if (Choose == 6) {
+            result = a*1000000;
+            cout<<"Your result is "<<result<<endl;
+        }
+        else if (Choose == 7) {
+            result = a*0.000247105407259363;
+            cout<<"your result is "<<result<<endl;
+        }
+        else if (Choose == 8) {
+            result = a*0.000000386102158542;
+            cout<<"Your result is "<<result<<endl;
+        }
+        else if (Choose == 9) {
+            result = a*1.195990046301080256;
+            cout<<"your result is "<<result<<endl;
+        }
+        else if (Choose == 10) {
+            result = a*10.76391041670972231;
+            cout<<"your result is "<<result<<endl;
+        }
+        else if (Choose == 11) {
+            result = a*1550.003100006200012;
+            cout<<"Your result is "<<result<<endl;
+        }
+        else if (Choose == 12) {
+            result = a*0.0039536861034746454;
+            cout<<"Your result is "<<result<<endl;
+        }
+        else {
+            cout<<"Invaild input"<<endl;
+        }
+        return 0;
+    }
+
+    static float Volume() {
+        float a;
+        int Choose;
+        float result;
+
+        cout<<"Welcome to Volume Converter";
+        cout<<"1. Cubic meter to cubic millimeter";
+        cout<<"2. Cubic meter to cubic decimeter";
+        cout<<"3. Cubic meter to cubic centimeter";
+        cout<<"4. Cubic meter to Hectoliter";
+        cout<<"5. Cubic meter to liter";
+        cout<<"6. Cubic meter to Deciliter";
+        cout<<"7. Cubic meter to Centiliter";
+        cout<<"8. Cubic meter to Millilitre";
+        cout<<"9. Cubic meter to Cubic foot";
+        cout<<"10. Cubic meter to Cubic inch";
+        cout<<"11. Cubic meter to cubic yard";
+        cout<<"12. Cubic meter to Acre-foot";
+        cout<<"Enter your choose";
+        cin>>Choose;
+        cout<<"Enter your Volume in cubic meter:- ";
+        cin>>a;
+
+        switch (Choose) {
+            case 1:
+                result = a*1000000000;
+                cout<<"Your result is "<<result<<endl;
+            case 2:
+                result = a*1000;
+                cout<<"Your result is "<<result<<endl;
+            case 3:
+                result = a*1000000;
+                cout<<"Your result is "<<result<<endl;
+            case 4:
+                result = a*10;
+                cout<<"Your result is "<<result<<endl;
+            case 5:
+                result = a*1000;
+                cout<<"Your result is "<<result<<endl;
+            case 6:
+                result = a*10000;
+                cout<<"Your result is "<<result<<endl;
+            case 7:
+                result = a*100000;
+                cout<<"Your result is "<<result<<endl;
+            case 8:
+                result = a*1000000;
+                cout<<"Your result is "<<result<<endl;
+            case 9:
+                result = a*35.31466672148859025;
+                cout<<"Your result is "<<result<<endl;
+            case 10:
+                result = a*61023.74409473228395;
+                cout<<"Your result is "<<result<<endl;
+            case 11:
+                result = a*1.307950619314392231;
+                cout<<"Your result is "<<result<<endl;
+            case 12:
+                result = a*0.000810372771474878;
+                cout<<"Your result is "<<result<<endl;
+            default:
+                cout<<"Invaild input";
+        }
+    }
+
+    static float Speed() {
+        float a;
+        int Choose;
+        float result;
+
+        cout<<"Welcome to Speed Convertor";
+        cout<<"1. Km/hr to Lightspeed";
+        cout<<"2. Km/hr to Mach";
+        cout<<"3. Km/hr to Meter/sec";
+        cout<<"4. Km/hr to Km/sec";
+        cout<<"5. Km/hr to Knot";
+        cout<<"6. Km/hr to Mile/hr";
+        cout<<"7. Km/hr to Foot/sec";
+        cout<<"8. Km/hr to Inch/sec";
+        cout<<"Enter your Choose:- ";
+        cin>>Choose;
+        cout<<"Enter speed in Km/hr:= ";
+        cin>>a;
+
+        switch (Choose) {
+            case 1:
+                result = a*0.000000000926566931;
+                cout<<"Your result is "<<result<<endl;
+            case 2;
+                result = a*0.000816273222973193;
+                cout<<"Your result is "<<result<<endl;
+            case 3:
+                result =
+            default:
+                cout<<"Invaild input";
+        }
     }
 };
 int main() {
